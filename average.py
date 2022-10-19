2dc08655f2d03212db77a000547ac3b123b00b6f
Marshall Demars <marshall.demars@mths.ca>
2022-10-19 14:28:23 +0000

    file

---
+++ average.py
@@ -0,0 +1,27 @@
+#!/usr/bin/env python3
+
+# Created by: Marshall Demars
+# Created on: October 2022
+# This program calculates the average of 3 numbers
+#    with user input
+
+
+def main():
+    # this calculates the average of 3 numbers
+
+    # input
+    number_one = float(input("Enter a number between 1 and 100: "))
+    number_two = float(input("Enter a number between 1 and 100: "))
+    number_three = float(input("Enter a number between 1 and 100: "))
+
+    # process and output
+    if user_number == random_number:
+        print("\nYou guessed correctly!")
+    else:
+        print("\nYou guessed incorrectly, the number was {}.".format(random_number))
+
+    print("\nDone.")
+
+
+if __name__ == "__main__":
+    main()
