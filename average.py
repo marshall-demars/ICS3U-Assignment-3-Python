4ea8a69140701712bb83d32846c1636db23b4080
Marshall Demars <marshall.demars@mths.ca>
2022-10-24 21:00:18 +0000

    py

--- average.py
+++ average.py
@@ -15,10 +15,21 @@ def main():
     number_three = float(input("Enter a number between 1 and 100: "))
 
     # process and output
-    if user_number == random_number:
-        print("\nYou guessed correctly!")
+    if number_one > 100:
+        print("\nPlease input a number between 1 and 100.")
+    elif number_two > 100:
+        print("\nPlease input a number between 1 and 100.")
+    elif number_three > 100:
+        print("\nPlease input a number between 1 and 100.")
+    elif number_one < 0:
+        print("\nPlease input a number between 1 and 100.")
+    elif number_two < 0:
+        print("\nPlease input a number between 1 and 100.")
+    elif number_three < 0:
+        print("\nPlease input a number between 1 and 100.")
     else:
-        print("\nYou guessed incorrectly, the number was {}.".format(random_number))
+        average = (number_one + number_two + number_three) / 3
+        print("\nThe average is {0}!".format(average))
 
     print("\nDone.")
 
